ff167032074ca1d555e7581b09da06095551d9ff
rushifer <74584550+neutron6@users.noreply.github.com>
2022-12-31 00:16:41 +0530

    Update EmployeeServiceImpl.java

--- ScrumProject/src/main/java/com/csi/ScrumProject/service/EmployeeServiceImpl.java
+++ ScrumProject/src/main/java/com/csi/ScrumProject/service/EmployeeServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class EmployeeServiceImpl {
+public class EmployeeServiceImpl implements EmailService {
     //****Scrum project by rushi Nichit
 
 
