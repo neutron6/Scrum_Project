85d2338ee545bd735aeb33534c380e9c6d49406e
rushifer <74584550+neutron6@users.noreply.github.com>
2023-01-01 22:01:55 +0530

    Delete ScrumProject directory

--- ScrumProject/src/main/java/com/csi/ScrumProject/exceptions/EmployeeRecordNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.csi.ScrumProject.exceptions;
-
-public class EmployeeRecordNotFoundException extends Exception {
-
-    EmployeeRecordNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- ScrumProject/src/main/java/com/csi/ScrumProject/exceptions/SignInException.java
+++
@@ -1,7 +0,0 @@
-package com.csi.ScrumProject.exceptions;
-
-public class SignInException extends Exception {
-    public SignInException(String m) {
-        super(m);
-    }
-}
